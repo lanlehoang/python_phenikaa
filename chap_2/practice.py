d14b6b76b9d85aed8112f9818cc2f58f201e12fe
lanlehoang <lelan210420@gmail.com>
2025-04-13 00:25:23 +0700

    done 6 first requirements

--- chap_2/practice.py
+++ chap_2/practice.py
@@ -1,10 +1,11 @@
-import numpy as np
-import uuid
+from utils.generators import StudentGenerator
+from utils.vi_dict_sorter import get_vi_name_key
+from typing import List, Optional, Callable
 
 
 class Student:
-    def __init__(self, name, gender, home, math, algo, prog):
-        self.id = str(uuid.uuid4())[:18]
+    def __init__(self, student_id, name, gender, home, math, algo, prog):
+        self.student_id = student_id
         self.name = name
         self.gender = gender
         self.home = home
@@ -13,183 +14,127 @@ class Student:
         self.prog = round(prog, 1)
         self.avg = round(sum([self.math, self.algo, self.prog])/3, 1)
 
+    def get_standardized_name(self):
+        return " ".join([n.capitalize() for n in self.name.split()])
+
     def get_upper_name(self):
         return self.name.upper()
     
     def get_lower_name(self):
         return self.name.lower()
     
-    def get_standardized_name(self):
-        name = self.name.split()
-        name = [n.capitalize() for n in name]
-        return " ".join(name)
-    
-    def get_western_name(self):
-        full_name = self.name.split()
-        return full_name[-1] + " " + " ".join(full_name[:-1])
-    
-    def print_student_info(self, name_type):
+    def print_student_info(self, formatted_name):
         print(
-            f"{self.id:<19} {name_type:<30} {self.gender:<6} "
+            f"{self.student_id:<14} {formatted_name:<30} {self.gender:<6} "
             f"{self.home:<15} {self.math:<5} {self.algo:<5} "
             f"{self.prog:<5} {self.avg:<5}"
         )
     
 
 class StudentManager:
-    def __init__(self, n_students):
-        self.students = []
-        self.n_students = n_students
+    def __init__(self, students: Optional[List[Student]]=None):
+        self.students = students if students else []
 
-        def generate_names_and_gender():
-            # Define constants
-            SURNAMES = ["Nguyễn", "Trần", "Lê", "Phạm", "Phan", "Bùi", "Huỳnh", "Hoàng"]
-            FEMALE_NAMES = [
-                "Khánh Linh", "Bảo Ngọc", "Thùy Trang", "Thu Hương", "Hồng Nhung", "Tường Vy",
-                "Ngọc Anh", "Thanh Hà", "Minh Châu", "Mai Phương", "Anh Thư", "Phương Nhi",
-                "Tú Uyên", "Lan Chi", "Hoài An", "Trúc Linh", "Hạ Vy", "Quỳnh Như",
-                "Thu Giang", "Kim Ngân", "Bích Phượng", "Diệp Anh", "Hải Yến", "Hà My"
-            ]
-            MALE_NAMES = [
-                "Minh Khoa", "Anh Duy", "Quốc Huy", "Gia Bảo", "Hoàng Phúc", "Tuấn Kiệt",
-                "Thành Đạt", "Nhật Minh", "Trung Hiếu", "Hoàng Nam", "Đức Anh", "Bảo Long",
-                "Khánh Duy", "Hải Đăng", "Ngọc Hưng", "Nam Khánh", "Đăng Khoa", "Tấn Phát",
-                "Tuấn Anh", "Đình Quân", "Chí Công", "Thái Sơn", "Gia Hưng", "Hoàng Lân"
-            ]
-            LENGTH_DISTRIBUTION = [0.15, 0.6, 0.25] # Probability of #2 #3 #4
-            LENGTH_NUMS = [int(dist*self.n_students) for dist in LENGTH_DISTRIBUTION[:-1]]
-            LENGTH_NUMS.append(self.n_students - sum(LENGTH_NUMS))
-            # Generate names
-            names = []
-            genders = []
-            # 2 letter names
-            for _ in range(LENGTH_NUMS[0]):
-                name = np.random.choice(SURNAMES) + " "
-                g = np.random.rand()   # 0 for Male, 1 for Female
-                if g < 0.5:
-                    gender = 'M'
-                    first_name = np.random.choice(MALE_NAMES).split()[-1]
-                else:
-                    gender = 'F'
-                    first_name = np.random.choice(FEMALE_NAMES).split()[-1]
-                name += first_name
+    @classmethod
+    def generate_students(cls, n_students):
+        # Generate names and genders, avoiding duplicated names
+        NAME_RETRIES = 3    # Number of retries to generate a unique name
+        names = []
+        genders = []
+        for _ in range(n_students):
+            exceeded_retries = True
+            for __ in range(NAME_RETRIES):
+                name, gender = StudentGenerator.generate_name_and_gender()
+                if name not in names:
+                    names.append(name)
+                    genders.append(gender)
+                    exceeded_retries = False
+                    break
+            if exceeded_retries:    # Add the name even if it was duplicated
                 names.append(name)
                 genders.append(gender)
-            # 3 letter names
-            for _ in range(LENGTH_NUMS[1]):
-                name = np.random.choice(SURNAMES) + " "
-                g = np.random.rand()   # 0 for Male, 1 for Female
-                if g < 0.5:
-                    gender = 'M'
-                    first_name = np.random.choice(MALE_NAMES)
-                else:
-                    gender = 'F'
-                    first_name = np.random.choice(FEMALE_NAMES)
-                name += first_name
-                names.append(name)
-                genders.append(gender)
-            # 4 letter names
-            for _ in range(LENGTH_NUMS[1]):
-                name = np.random.choice(SURNAMES) + " "
-                g = np.random.rand()   # 0 for Male, 1 for Female
-                if g < 0.5:
-                    gender = 'M'
-                    first_name = "Văn " + np.random.choice(MALE_NAMES)
-                else:
-                    gender = 'F'
-                    first_name = "Thị " + np.random.choice(FEMALE_NAMES)
-                name += first_name
-                names.append(name)
-                genders.append(gender)
-            return names, genders
-        
-        def generate_home():
-            PROVINCES = ["Hà Nội", "Thái Bình", "Nam Định", "Hải Phòng", "Hải Dương"]
-            return [np.random.choice(PROVINCES) for _ in range(self.n_students)]
-        
-        def generate_scores():
-            return np.random.uniform(4, 10, self.n_students)
-        
-        names, genders = generate_names_and_gender()
-        home = generate_home()
-        math_scores = generate_scores()
-        algo_scores = generate_scores()
-        prog_scores = generate_scores()
+        # Generate the remaining attributes
+        student_ids = [StudentGenerator.generate_student_id() for _ in range(n_students)]
+        homes = [StudentGenerator.generate_home() for _ in range(n_students)]
+        math_scores = [StudentGenerator.generate_score() for _ in range(n_students)]
+        algo_scores = [StudentGenerator.generate_score() for _ in range(n_students)]
+        prog_scores = [StudentGenerator.generate_score() for _ in range(n_students)]
+        # Create Student objects
+        return cls([
+            Student(
+                student_id=student_ids[i],
+                name=names[i],
+                gender=genders[i],
+                home=homes[i],
+                math=math_scores[i],
+                algo=algo_scores[i],
+                prog=prog_scores[i]
+            ) for i in range(n_students)
+        ])
 
-        for i in range(self.n_students):
-            self.students.append(
-                Student(names[i], genders[i], home[i],
-                        math_scores[i], algo_scores[i], prog_scores[i])
-            )
+    @classmethod
+    def from_file(cls, filename):
+        pass
 
-    def print_header(self):
+    def to_file(self, filename):
+        pass
+
+    def print_list(self, name_getter: Callable[[Student], str]):
         print(
-            f"{'ID':<19} {'Name':<30} {'Gender':<6} {'Home':<15} "
+            f"{'ID':<14} {'Name':<30} {'Gender':<6} {'Home':<15} "
             f"{'Math':<5} {'Algo':<5} {'Prog':<5} {'Avg':<5}"
         )
-        print("-" * 95)
-
-    def print_list_upper(self):
-        self.print_header()
+        print("-"*90)
         for student in self.students:
-            student.print_student_info(student.get_upper_name())
+            student.print_student_info(name_getter(student))
+        print("-"*90)
 
-    def print_list_lower(self):
-        self.print_header()
-        for student in self.students:
-            student.print_student_info(student.get_lower_name())
+    def sort_by_name(self):
+        self.students.sort(key=lambda s: get_vi_name_key(s.name))
 
-    def print_list_standardized(self):
-        self.print_header()
-        for student in self.students:
-            student.print_student_info(student.get_standardized_name())
+    def sort_by_home(self):
+        self.students.sort(key=lambda s: s.home)
 
-    def print_sorted_names(self):
-        self.print_header()
-        for student in sorted(self.students, key=lambda s: s.get_western_name()):
-            student.print_student_info(student.get_standardized_name())
-
-    def print_sorted_home(self):
-        self.print_header()
-        for student in sorted(self.students, key=lambda s: s.home):
-            student.print_student_info(student.get_standardized_name())
-
-    def print_sorted_score(self, subject):
+    def sort_by_score(self, subject):
         if subject not in ("math", "algo", "prog", "avg"):
             raise ValueError("Invalid subject")
-        self.print_header()
-        for student in sorted(self.students, key=lambda s: getattr(s, subject), reverse=True):
-            student.print_student_info(student.get_standardized_name())         
+        self.students.sort(key=lambda s: getattr(s, subject), reverse=True) 
 
 
 if __name__ == "__main__":
     n_students = int(input().strip())
-    students = StudentManager(n_students)
+    students = StudentManager.generate_students(n_students)
     print("1. Print list of students with upper case names")
-    students.print_list_upper()
+    students.print_list(Student.get_upper_name)
     print("\n\n")
     print("2. Print list of students with lower case names")
-    students.print_list_lower()
+    students.print_list(Student.get_lower_name)
     print("\n\n")
     print("3. Print list of students with standardized names")
-    students.print_list_standardized()
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("4. Print list of students with sorted names")
-    students.print_sorted_names()
+    students.sort_by_name()
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("5. Print list of students with sorted home towns")
-    students.print_sorted_home()
+    students.sort_by_home()
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("6. Print list of students with sorted math scores")
-    students.print_sorted_score("math")
+    students.sort_by_score("math")
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("7. Print list of students with sorted algorithm scores")
-    students.print_sorted_score("algo")
+    students.sort_by_score("algo")
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("8. Print list of students with sorted programming scores")
-    students.print_sorted_score("prog")
+    students.sort_by_score("prog")
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     print("9. Print list of students with sorted average scores")
-    students.print_sorted_score("avg")
+    students.sort_by_score("avg")
+    students.print_list(Student.get_standardized_name)
     print("\n\n")
     
